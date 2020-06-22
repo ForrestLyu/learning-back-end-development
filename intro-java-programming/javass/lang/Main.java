37882be132ce9cbfb53e9144c5008fb4799c63ba
forrest lyu <forrest.lyu.dev@gmail.com>
2020-06-22 08:07:48 +0800

    Main.java

---
+++ intro-java-programming/javass/lang/Main.java
@@ -0,0 +1,19 @@
+package javass.lang;
+
+public class Main {
+	
+		/**
+		 * In main() method, I will try:
+		 * 0. OpenJDK (JVM, javac, java, javadoc.
+		 * 1. Variables, data types, basic operators, input
+		 * &amp;scanner, conditions (if, else, else if), nested
+		 * statements, loops (for, while, do while).
+		 * 2. Collections: arrays, sets, lists, map, hashmap.
+		 * 3. Oriented-Object: abstraction (creating classes, inner
+		 * classes), inheritance (overloading methods, interface),
+		 * enums, polymorphism.
+		 */
+	public static void main(String[] args) {
+
+	}
+}
