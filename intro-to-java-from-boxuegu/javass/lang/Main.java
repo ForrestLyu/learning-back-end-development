028f978fb9f29316e771829b65a3863295b8e980
forrest lyu <forrest.lyu.dev@gmail.com>
2020-06-22 08:46:48 +0800

    update

--- intro-to-java-from-boxuegu/javass/lang/Main.java
+++ intro-to-java-from-boxuegu/javass/lang/Main.java
@@ -3,7 +3,11 @@ package javass.lang;
 public class Main {
 
 	/**
-	 * 1. Intro. to 
+	 * 1. Intro. to java.
+	 * 2. Basic grammar
+	 * 3. Conditions &amp; loops statement.
+	 * 4. Integrated Develoment Environment &amp; Vim + Maven
+	 * 5. Collections
 	 */
 
 	public static void main(String[] args) {
