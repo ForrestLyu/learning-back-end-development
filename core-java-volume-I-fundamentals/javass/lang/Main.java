c43084dd0428f55fa5745c184e10077c892bf826
forrest lyu <forrest.lyu.dev@gmail.com>
2020-06-23 13:09:40 +0800

    update

---
+++ core-java-volume-I-fundamentals/javass/lang/Main.java
@@ -0,0 +1,16 @@
+package javass.lang;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int i = 9;
+		int j = 3;
+		i = i > j ?  999 : j;
+
+		System.out.println(i);
+
+	}
+
+}
