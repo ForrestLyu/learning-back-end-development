79471df5aefdc1ff2c8039c7937833dd965fc884
forrest lyu <forrest.lyu.dev@gmail.com>
2020-06-23 08:09:00 +0800

    update

--- intro-to-java-from-boxuegu/javass/lang/Main.java
+++ intro-to-java-from-boxuegu/javass/lang/Main.java
@@ -13,17 +13,8 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-		int i = (int)88.88;
-
-		int j = (int)Math.round(88.49);
-
-		int k = (int)Math.round(88.5);
-
-		int p = (int)Math.round(88.9);
+		byte i = (byte)128;
 
 		System.out.println(i);
-		System.out.println(j);
-		System.out.println(k);
-		System.out.println(p);
 	}
 }
